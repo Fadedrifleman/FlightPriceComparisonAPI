cb5f36174dab7352f4e0a5abdd27d51e78de026d
Shivam Uttam <102919037+Fadedrifleman@users.noreply.github.com>
2023-05-03 20:15:51 +0530

    added routes

--- routes/main.js
+++ routes/main.js
@@ -3,5 +3,6 @@ const router = express.Router();
 import { getHome, getPrices } from "../controllers/flight.js";
 
 router.get("/", getHome);
+router.post("/flight", getPrices);
 
 export default router;
